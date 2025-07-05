379b43aa1b52a7eef3e18b2d986f645ab187afae
anturated <anturated@gmail.com>
2025-07-05 15:46:48 +0200

    Initial Commit

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+source ~/.config/fish/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    echo ""
+    fastfetch
+    echo ""
+    echo ""
+end
+
+
+alias dalarm="cd ~/Documents/projects/alarm && source bin/activate.fish && python main.py"
+alias yay="paru"
+
+starship init fish | source
+zoxide init fish | source
